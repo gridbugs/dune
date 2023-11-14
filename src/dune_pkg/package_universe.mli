9374710ec269207b2cc149fdade0b00a8d3cd421
Stephen Sherratt <stephen@sherra.tt>
2023-11-14 12:21:21 +1100

    pkg: validate lockdir against local packages
    This adds a type `Dune_pkg.Package_universe.t` made up of the set of
    local packages in a project and a lockdir whose constructor validates
    that the lockdir contains a valid solution for the dependencies of local
    packages. As we add more pkg operations we'll need a way of talking
    about the set of packages that makes up a project, both local and not.
    
    Also this adds a command `dune pkg validate-lockdir` which validates
    lockdirs against local packages in order to test this feature.
    
    Signed-off-by: Stephen Sherratt <stephen@sherra.tt>

---
+++ src/dune_pkg/package_universe.mli
@@ -0,0 +1,26 @@
+open! Import
+
+(** All of the packages in a dune project including local packages and packages
+    in a lockdir. The lockdir is guaranteed to contain a valid dependency
+    solution for the local packages. *)
+type t = private
+  { local_packages : Local_package.t Package_name.Map.t
+  ; lock_dir : Lock_dir.t
+  }
+
+module Error : sig
+  type t =
+    | Duplicate_package_between_local_packages_and_lock_dir of
+        { local_package : Local_package.t
+        ; locked_package : Lock_dir.Pkg.t
+        }
+    | Local_package_dependencies_unsatisfied_by_lock_dir of
+        { local_package : Local_package.t
+        ; hints : Resolve_opam_formula.Unsatisfied_formula_hint.t list
+        }
+    | Unneeded_packages_in_lock_dir of Lock_dir.Pkg.t list
+
+  val to_user_message : t -> User_message.t
+end
+
+val create : Local_package.t Package_name.Map.t -> Lock_dir.t -> (t, Error.t) result
