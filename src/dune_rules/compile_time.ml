830788e4a3e66165e28cea810109aa72d744af4e
Stephen Sherratt <stephen@sherra.tt>
2024-12-04 18:10:55 +1100

    Windows hacks
    Signed-off-by: Stephen Sherratt <stephen@sherra.tt>

--- src/dune_rules/compile_time.ml
+++ src/dune_rules/compile_time.ml
@@ -1,9 +1,5 @@
 open Import
 
 let toolchains = Config.make_toggle ~name:"toolchains" ~default:Setup.toolchains
-
-let pkg_build_progress =
-  Config.make_toggle ~name:"pkg_build_progress" ~default:Setup.pkg_build_progress
-;;
-
+let pkg_build_progress = Config.make_toggle ~name:"pkg_build_progress" ~default:`Enabled
 let lock_dev_tools = Config.make_toggle ~name:"lock_dev_tool" ~default:Setup.lock_dev_tool
