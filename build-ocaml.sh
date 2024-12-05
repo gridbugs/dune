dedd1299bfc3ab20cc6cb73174fff7c94d5894e7
Stephen Sherratt <stephen@sherra.tt>
2024-12-05 19:04:27 +1100

    Updates

--- build-ocaml.sh
+++ build-ocaml.sh
@@ -2,7 +2,7 @@
 
 COMPILER_VERSION=5.2.1
 pushd $(mktemp -d)
-opam source ocaml-base-compiler.COMPILER_VERSION
+opam source ocaml-base-compiler.$COMPILER_VERSION
 opam source flexdll.0.43
 
 # apply a patch to flexdll to work around an issue between flexdll and the x86_64-w64-mingw32 toolchain
@@ -15,10 +15,10 @@ winpath() {
 }
 
 # configure and build the compiler
-cd ocaml-base-compiler.COMPILER_VERSION
+cd ocaml-base-compiler.$COMPILER_VERSION
 # Set this to the hash of the ocaml-base-compiler.pkg file in your lockdir. Find this out by trying to build the compiler package with dune and watch the verbose output.
 DUNE_TOOLCHAIN_HASH=ec2fb9e295c43061b4ea55894f8a2d08
 TARGET=x86_64-w64-mingw32
-./configure --host=$TARGET --build=$TARGET --target=$TARGET --enable-imprecise-c99-float-ops --prefix=$(winpath $HOME/AppData/Local/Microsoft/Windows/INetCache/dune/toolchains/ocaml-base-compiler.COMPILER_VERSION-$DUNE_TOOLCHAIN_HASH/target) --docdir=$(winpath $HOME/AppData/Local/Microsoft/Windows/INetCache/dune/toolchains/ocaml-base-compiler.COMPILER_VERSION-$DUNE_TOOLCHAIN_HASH/target/doc/ocaml) --with-flexdll=$(realpath $(pwd)/../flexdll.0.43) -C
+./configure --host=$TARGET --build=$TARGET --target=$TARGET --enable-imprecise-c99-float-ops --prefix=$(winpath $HOME/AppData/Local/Microsoft/Windows/INetCache/dune/toolchains/ocaml-base-compiler.$COMPILER_VERSION-$DUNE_TOOLCHAIN_HASH/target) --docdir=$(winpath $HOME/AppData/Local/Microsoft/Windows/INetCache/dune/toolchains/ocaml-base-compiler.$COMPILER_VERSION-$DUNE_TOOLCHAIN_HASH/target/doc/ocaml) --with-flexdll=/c/Users/s/src/ocaml/flexdll -C
 make -j
 make install
