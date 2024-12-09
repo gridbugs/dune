25ea27b4d7d287a10919071b264d8b4505a19956
Stephen Sherratt <stephen@sherra.tt>
2024-12-09 18:14:07 +1100

    Comments

--- build-ocaml.sh
+++ build-ocaml.sh
@@ -1,16 +1,29 @@
 #!/usr/bin/env bash
 
+# Script to build and install the compiler in a similar manner to dune
+# toolchains. This can be helpful when debugging issues with dune toolchains.
+# It also allows one to use dune to build a project that depends on the
+# compiler when support for building the compiler with dune toolchains is
+# broken or not yet implemented, as dune will see compilers installed using
+# this script and be able to use them as though they had been installed with
+# dune toolchains.
+
 COMPILER_VERSION=5.2.1
 FLEXDLL_VERSION=0.43
 pushd $(mktemp -d)
 opam source ocaml-base-compiler.$COMPILER_VERSION
 opam source flexdll.$FLEXDLL_VERSION
 
+# Convert a cygwin-style path (e.g. "/c/foo/bar") into a windows-style path
+# (e.g. "C:/foo/bar"). The resulting ptah will still use slashes as delimiters
+# rather than forward slashes. Only works if the path in inside the C-drive.
 winpath() {
   echo $1 | sed 's/^\/c/C:/'
 }
 
-# Set this to the hash of the ocaml-base-compiler.pkg file in your lockdir. Find this out by trying to build the compiler package with dune and watch the verbose output.
+# Set this to the hash of the ocaml-base-compiler.pkg file in your lockdir.
+# Find this out by trying to build the compiler package with dune and watch the
+# verbose output.
 DUNE_TOOLCHAIN_HASH=ec2fb9e295c43061b4ea55894f8a2d08
 PREFIX=$(winpath $HOME/AppData/Local/Microsoft/Windows/INetCache/dune/toolchains/ocaml-base-compiler.$COMPILER_VERSION-$DUNE_TOOLCHAIN_HASH/target)
 
