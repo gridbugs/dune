71b3a5cc21e8e7518da3e1be2c59c9e35eb67654
Stephen Sherratt <stephen@sherra.tt>
2025-02-11 15:41:38 +1100

    Portable lockdirs
    Signed-off-by: Stephen Sherratt <stephen@sherra.tt>

--- src/dune_pkg/file_entry.ml
+++ src/dune_pkg/file_entry.ml
@@ -4,7 +4,18 @@ type source =
   | Path of Path.t
   | Content of string
 
+let source_equal a b =
+  match a, b with
+  | Path a, Path b -> Path.equal a b
+  | Content a, Content b -> String.equal a b
+  | Path _, Content _ | Content _, Path _ -> false
+;;
+
 type t =
   { original : source
   ; local_file : Path.Local.t
   }
+
+let equal { original; local_file } t =
+  source_equal original t.original && Path.Local.equal local_file t.local_file
+;;
--- src/dune_pkg/file_entry.mli
+++ src/dune_pkg/file_entry.mli
@@ -8,3 +8,5 @@ type t =
   { original : source
   ; local_file : Path.Local.t
   }
+
+val equal : t -> t -> bool
